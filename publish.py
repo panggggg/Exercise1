7ef2cb18553b9c551549ca79490b956a0e764d07
panggggg <p.picczy@gmail.com>
2021-05-10 14:16:42 +0700

    connect rabbitMQ

---
+++ publish.py
@@ -0,0 +1,14 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+channel = connection.channel()
+
+channel.exchange_declare(exchange="exercise", exchange_type="fanout")
+
+print("Name:")
+name = input()
+
+channel.basic_publish(exchange="exercise", routing_key="", body=name)
+print("[X] Sent data message")
+
+connection.close()
