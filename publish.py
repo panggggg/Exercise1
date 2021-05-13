e0b1f2396e593a2e03d5e0b243ca52f4b2109c97
panggggg <p.picczy@gmail.com>
2021-05-13 19:06:21 +0700

    edit input

--- publish.py
+++ publish.py
@@ -1,16 +1,26 @@
 import pika
+import json
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+connection = pika.BlockingConnection(pika.ConnectionParameters("localhost", port=5673))
 channel = connection.channel()
 
 channel.exchange_declare(exchange="exercise", exchange_type="fanout")
 
-print("Name:")
-name = input()
-print(type(name))
+info = {}
 
-channel.basic_publish(exchange="exercise", routing_key="", body=name)
+
+print("Username:")
+username = input()
+info["username"] = username
+
+print("Email:")
+email = input()
+info["email"] = email
+
+print(info)
+
+channel.basic_publish(exchange="exercise", routing_key="", body=json.dumps(info))
 print("[X] Sent data message")
 
 connection.close()
