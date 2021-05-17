8d3f7c502c4aeef37682680f5f29629c3774c5c7
panggggg <p.picczy@gmail.com>
2021-05-17 14:41:16 +0700

    no message

---
+++ config/development.py
@@ -0,0 +1,11 @@
+config = {
+    "mongo_config": {
+        "host": "localhost",
+        "port": 27018,
+        "user": "root",
+        "password": "root",
+        "auth_db": "admin",
+        "db": "mydb",
+        "collection": "people",
+    }
+}
---
+++ database/mongodb.py
@@ -0,0 +1,26 @@
+from logging import info
+import pymongo
+
+
+class MongoDB:
+    def __init__(self, host, port, user, password, auth_db, db, collection):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.auth_db = auth_db
+        self.db = db
+        self.collection = collection
+        self.connection = None
+
+    def _connect(self):
+        client = pymongo.MongoClient(
+            host=self.host,
+            port=self.port,
+            username=self.user,
+            password=self.password,
+            authSource=self.auth_db,
+            authMechanism="SCRAM-SHA-1",
+        )
+        db = client[self.db]
+        self.connection = db[self.collection]
